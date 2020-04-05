4e80e08681bc20f001285daf578f7ce9671745f6
geoffhignett <geoff.hignett@xigen.co.uk>
2020-04-05 13:23:52 +0100

    count up functionality added

--- js/index.js
+++ js/index.js
@@ -10,3 +10,38 @@ hamburger.addEventListener('click', () => {
 		hamburgerOpen = false;
 	}
 });
+
+$( document ).ready(function() {
+	$counters = $('.count-up');
+
+	$counters.each((index, el) => {
+		let $counter, countTo, time;
+
+		$counter = $(el);
+
+		// If has not fired and on screen
+		if (!$counter.hasClass('count-done')) {
+			$counter.html("0");
+
+			countTo = $counter.attr('data-to');
+			time = parseInt($counter.attr('data-time'));
+
+			$({countNum: "0"}).animate({
+					countNum: countTo
+				},
+				{
+					duration: time,
+					easing: 'linear',
+					step: function () {
+						$counter.text(new Number(this.countNum).toLocaleString('en-GB', {maximumFractionDigits: 0}));
+					},
+					complete: function () {
+						$counter.text(new Number(this.countNum).toLocaleString('en-GB', {maximumFractionDigits: 0}));
+					}
+
+				});
+			$counter.addClass('count-done');
+		}
+	})
+
+});
