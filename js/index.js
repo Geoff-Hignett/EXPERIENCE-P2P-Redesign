4971d1e4e4769d19fcfb8b613777806d8418cb97
geoffhignett <geoff.hignett@xigen.co.uk>
2020-04-11 15:31:59 +0100

    nav restyle, home page responsive

--- js/index.js
+++ js/index.js
@@ -1,20 +1,26 @@
 // hamburger
 const hamburger = document.querySelector('.navbar__toggle-label');
+const heroContent = document.querySelector('.hero__content');
 let hamburgerOpen = false;
+let heroContentDown = false;
 
 hamburger.addEventListener('click', () => {
 	if (!hamburgerOpen) {
 		hamburger.classList.add('open');
+		heroContent.classList.add('open');
 		hamburgerOpen = true;
+		heroContentDown = true;
 	} else {
 		hamburger.classList.remove('open');
+		heroContent.classList.remove('open');
 		hamburgerOpen = false;
+		heroContentDown = false;
 	}
 });
 
 
 // stats counter
-const statsCount = document.querySelector('.stats-banner');
+const statsCount = document.querySelector('.in-numbers');
 
 const config = {
   threshold: 0.8
